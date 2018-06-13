24770587ff4586e93104852c0c283fd0eef7f750
Sam Vevang <sam.vevang@gmail.com>
2018-06-13 11:23:00 -0500

    new create_trie method

--- lib/trie.ex
+++ lib/trie.ex
@@ -3,21 +3,67 @@ defmodule Trie do
   Trie CRUD.
   """
 
-  @page_size 4096
+  @doc """
+  Given an Elixir binary, return a trie representation whose bits are edges in a binary tree.
+  Input keys traverse the tree with 0 for left and 1 for right.
+  """
+  def from_key(input_key) when is_binary(input_key) == false or byte_size(input_key) == 0 do
+    raise "Input key must consist of whole bytes."
+  end
+
+  def from_key(input_key) do
+    do_from_key(input_key)
+  end
+
+  def do_from_key(input_key, acc \\ [])
+
+  # done recursing
+  def do_from_key(input_key, acc) when bit_size(input_key) == 0 do
+    Enum.reverse(acc)
+  end
+
+  def do_from_key(input_key, acc) do
+    <<target_bit::size(1), rest::bitstring>> = input_key
+
+    acc = case target_bit do
+      1 -> [right_branch | acc]
+      0 -> [left_branch | acc]
+    end
+
+    do_from_key(rest, acc)
+  end
+
+  def as_list(trie) do
+    trie
+    |> Enum.map(fn(bits)-> for <<b :: 1 <- bits  >>, do: b end)
+  end
+
+  def left_branch do
+    <<1::size(1), 0::size(1)>>
+  end
+
+  def right_branch do
+    <<0::size(1), 1::size(1)>>
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-  iex> {:ok, _file} = Trie.create_unsorted([], "/tmp/test.trie")
-  iex> :ok
-  :ok
-
+  iex> Trie.bit_at_index(<<255>>, 7)
+  1
+  iex> Trie.bit_at_index(<<254>>, 7)
+  0
   """
+  def bit_at_index(bin, index) when is_binary(bin) == false
+                               when byte_size(bin) == 0
+                               when index < 0
+                               when index >= bit_size(bin) do
+    raise "Bad argument"
+  end
 
-  def create_unsorted(key_list, dest) do
-    {:ok, trie_file} = File.open(dest, [:write])
+  def bit_at_index(bin, index) do
+    leading = index
+    trailing = bit_size(bin) - leading - 1
+    <<_::size(leading), target_bit::size(1), _::size(trailing) >> = bin
+    target_bit
   end
 
 end
--- test/trie_test.exs
+++ test/trie_test.exs
@@ -2,10 +2,30 @@ defmodule TrieTest do
   use ExUnit.Case
   doctest Trie
 
-  describe "create_unsorted/2" do
+  describe "from_keye/1" do
     test "Sets up a new trie" do
-      words = ["another", "fine", "test"]
-      {:ok, trie_file} = Trie.create_unsorted(words, "/tmp/test.trie")
+      a_byte = <<97>>
+      assert (Trie.from_key(a_byte) |> Trie.as_list) == [[1, 0], [0, 1], [0, 1], [1, 0], [1, 0], [1, 0], [1, 0], [0, 1]]
     end
+
+    test "empty byte" do
+      all_zero_byte = <<0::1, 0::1, 0::1, 0::1, 0::1, 0::1, 0::1, 0::1>>
+      assert (Trie.from_key(all_zero_byte) |> Trie.as_list) == [[1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0]]
+    end
+
+    test "filled byte" do
+      all_one_byte = <<1::1, 1::1, 1::1, 1::1, 1::1, 1::1, 1::1, 1::1>>
+      assert (Trie.from_key(all_one_byte) |> Trie.as_list) == [[0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1]]
+    end
+
+    test "only accepts keys composed of whole bytes" do
+      assert_raise RuntimeError, ~r/^Input key must consist of whole bytes.$/, fn ->
+        Trie.from_key(<<1::size(1)>>)
+      end
+      assert_raise RuntimeError, ~r/^Input key must consist of whole bytes.$/, fn ->
+        Trie.from_key(<<>>)
+      end
+    end
+
   end
 end
